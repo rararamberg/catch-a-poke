7e8e792c663a849bdba6abc27da426721b8a40f4
Rachel Ramberg <rachelramberg@Rachels-MacBook-Pro.local>
2021-11-11 22:43:21 -0600

    Laid steps comments in script.js

--- script.js
+++ script.js
@@ -2,3 +2,28 @@
 
 // 1. User clicks play button
 // random pokemon sprites render on background screen
+// pokemon move around within container of div
+
+//2. User Clicks on pokemon
+//pokemon becomes pokeball
+//pokemon removed from screen
+// pokemon is add to "collection"
+
+//3. user clicks on last pokemon in game
+// collection not hidden
+// user sees pokemon they collected
+// play button says "new game"
+
+//4. User clicks on pokemon card in collection
+// API request(s) made to poke API
+// Poke data appends to modal window
+// modal and overlay is visible
+
+//5. User clicks out of or clicks exit button
+//modal and overlay hidden
+// previous data removed to allow new poke to render when clicked
+
+//6. User clicks "new game" button (play)
+//previous data in collection removed
+// collection div hidden
+//new game begins
